764bd9aa1aa1f4a1c6331bf6bad8e1fc7deb9786
ads-blocker <goran.stambuk.zadar@gmail.com>
2026-01-02 19:41:11 +0100

    Add files via upload

--- Iso/sources/$OEM$/$$/Setup/Scripts/Bin/Antivirus.ps1
+++ Iso/sources/$OEM$/$$/Setup/Scripts/Bin/Antivirus.ps1
@@ -1946,20 +1946,7 @@ function Invoke-YouTubeAdBlocker {
 
             $ErrorActionPreference = "Continue"
 
-            $BlockedDomains = @(
-                "doubleclick.net",
-                "googlesyndication.com",
-                "googleadservices.com",
-                "google-analytics.com",
-                "2mdn.net",
-                "youtube.com/api/stats/ads",
-                "youtube.com/pagead/",
-                "youtube.com/ptracking",
-                "youtube.com/api/stats/qoe",
-                "s.youtube.com/api/stats/qoe",
-                "static.doubleclick.net"
-            )
-
+            # YouTube-specific JavaScript injection
             $YouTubeScriptlet = @"
 <script>
 (function() {
@@ -1989,14 +1976,6 @@ function Invoke-YouTubeAdBlocker {
 </script>
 "@
 
-            function Test-BlockedDomain {
-                param([string]$Url)
-                foreach ($domain in $BlockedDomains) {
-                    if ($Url -like "*$domain*") { return $true }
-                }
-                return $false
-            }
-
             function Inject-YouTubeScript {
                 param([string]$HtmlContent)
                 if ($HtmlContent -match '</head>') {
@@ -2023,12 +2002,6 @@ function Invoke-YouTubeAdBlocker {
                     }
 
                     $targetUrl = if ($requestUrl -match '^http') { $requestUrl } else { "http://$($request.Headers['Host'])$requestUrl" }
-
-                    if (Test-BlockedDomain -Url $targetUrl) {
-                        $response.StatusCode = 204
-                        $response.Close()
-                        return
-                    }
 
                     $webRequest = [System.Net.HttpWebRequest]::Create($targetUrl)
                     $webRequest.Method = $method
@@ -2110,14 +2083,255 @@ function Invoke-YouTubeAdBlocker {
 
         $pacFile = "$env:TEMP\youtube-adblocker.pac"
         $pacContent = @"
+// Comprehensive Ad-Blocking with YouTube JS Injection Proxy
+// Based on BlockAds.txt with YouTube-specific routing
+
+// Configuration Variables
+var normal = "DIRECT";
+var blackhole = "PROXY 127.0.0.1:3421";  // Ad-blocking blackhole proxy
+var youtubeProxy = "PROXY 127.0.0.1:8080";  // YouTube JS injection proxy
+var isEnabled = 1;
+var debug = 0;
+
+// Whitelist: Domains explicitly allowed
+var whitelist = [
+    "twitter.com",
+    "x.com",
+    "perplexity.ai",
+    "mediafire.com",
+    "apple.com",
+    "schooner.com",
+    "citibank.com",
+    "ebay.com",
+    "yahoo.com",
+    "discord.com",
+    "click.discord.com",
+    "discordapp.com",
+    "cdn.discordapp.com",
+    "cdn.discord.app",
+    "discord.gg",
+    "discord.media",
+    "discordapp.net",
+    "media.discordapp.net",
+    "discordstatus.com",
+    "dis.gd",
+    "discordcdn.com",
+    "aliexpress.com",
+    "tenor.com",
+    "media.tenor.com"
+];
+
+// Comprehensive Regular Expression for Ad/Tracking Domains and Subdomains
+var adDomainRegex = /^(?:.*[-_.])?(ads?|adv(ert(s|ising)?)?|banners?|track(er|ing|s)?|beacons?|doubleclick|adservice|adnxs|adtech|googleads|gads|adwords|partner|sponsor(ed)?|click(s|bank|tale|through)?|pop(up|under)s?|promo(tion)?|market(ing|er)?|affiliates?|metrics?|stat(s|counter|istics)?|analytics?|pixel(s)?|campaign|traff(ic|iq)|monetize|syndicat(e|ion)|revenue|yield|impress(ion)?s?|conver(sion|t)?|audience|target(ing)?|behavior|profil(e|ing)|telemetry|survey|poll|outbrain|taboola|quantcast|scorecard|omniture|comscore|krux|bluekai|exelate|adform|adroll|rubicon|vungle|inmobi|flurry|mixpanel|heap|amplitude|optimizely|bizible|pardot|hubspot|marketo|eloqua|salesforce|media(math|net)|criteo|appnexus|turn|adbrite|admob|adsonar|adscale|zergnet|revcontent|mgid|nativeads|contentad|displayads|bannerflow|adblade|adcolony|chartbeat|newrelic|pingdom|gauges|kissmetrics|webtrends|tradedesk|bidder|auction|rtb|programmatic|splash|interstitial|overlay)\./i;
+
+// Regular Expression for Ad-Related URL Patterns and XSS Blocking
+var adUrlRegex = /(?:\/(?:adcontent|img\/adv|web\-ad|iframead|contentad|ad\/image|video\-ad|stats\/event|xtclicks|adscript|bannerad|googlead|adhandler|adimages|embed\-log|adconfig|tracking\/track|tracker\/track|adrequest|nativead|adman|advertisement|adframe|adcontrol|adoverlay|adserver|adsense|google\-ads|ad\-banner|banner\-ad|campaign\/advertiser|adplacement|adblockdetect|advertising|admanagement|adprovider|adrotation|adtop|adbottom|adleft|adright|admiddle|adlarge|adsmall|admicro|adunit|adcall|adlog|adcount|adserve|adsrv|adsys|adtrack|adview|adwidget|adzone|banner\/adv|google_tag|image\/ads|sidebar\-ads|footer\-ads|top\-ads|bottom\-ads|new\-ads|search\-ads|lazy\-ads|responsive\-ads|dynamic\/ads|external\/ads|mobile\-ads|house\-ads|blog\/ads|online\/ads|pc\/ads|left\-ads|right\-ads|ads\/square|ads\/text|ads\/html|ads\/js|ads\.php|ad\.js|ad\.css|\?affiliate=|\?advertiser=|\&adspace=|\&adserver=|\&adgroupid=|\&adpageurl=|\.adserve|\.ads\d|\.adspace|\.adsense|\.adserver|\.google\-ads|\.banner\-ad|\.ad\-banner|\.adplacement|\.advertising|\.admanagement|\.adprovider|\.adrotation|\.adtop|\.adbottom|\.adleft|\.adright|\.admiddle|\.adlarge|\.adsmall|\.admicro|\.adunit|\.adcall|\.adlog|\.adcount|\.adserve|\.adsrv|\.adsys|\.adtrack|\.adview|\.adwidget|\.adzone|xss))/i;
+
+// Regular Expression for Common Ad Subdomains
+var adSubdomainRegex = /^(?:adcreative(s)?|imageserv|media(mgr)?|stats|switch|track(2|er)?|view|ad(s)?\d{0,3}|banner(s)?\d{0,3}|click(s)?\d{0,3}|count(er)?\d{0,3}|servedby\d{0,3}|toolbar\d{0,3}|pageads\d{0,3}|pops\d{0,3}|promos\d{0,3})\./i;
+
+// Regular Expression for Web Bugs and Flash Ads
+var adWebBugRegex = /(?:\/(?:1|blank|b|clear|pixel|transp|spacer)\.gif|\.swf)$/i;
+
+// Blacklist: Explicitly blocked domains
+var blacklist = [
+    "adtago.s3.amazonaws.com",
+    "analyticsengine.s3.amazonaws.com",
+    "advice-ads.s3.amazonaws.com",
+    "affiliationjs.s3.amazonaws.com",
+    "advertising-api-eu.amazon.com",
+    "ssl.google-analytics.com",
+    "fastclick.com",
+    "fastclick.net",
+    "media.fastclick.net",
+    "cdn.fastclick.net",
+    "analytics.yahoo.com",
+    "global.adserver.yahoo.com",
+    "ads.yap.yahoo.com",
+    "appmetrica.yandex.com",
+    "yandexadexchange.net",
+    "analytics.mobile.yandex.net",
+    "extmaps-api.yandex.net",
+    "adsdk.yandex.ru",
+    "hotjar.com",
+    "static.hotjar.com",
+    "api-hotjar.com",
+    "jotjar-analytics.com",
+    "mouseflow.com",
+    "freshmarketer.com",
+    "luckyorange.com",
+    "cdn.luckyorange.com",
+    "w1.luckyorange.com",
+    "upload.luckyorange.com",
+    "cs.luckyorange.com",
+    "settings.luckyorange.com",
+    "stats.wp.com",
+    "app.bugsnag.com",
+    "api.bugsnag.com",
+    "notify.bugsnag.com",
+    "sessions.bugsnag.com",
+    "browser.sentry-cdn.com",
+    "app.getsentry.com",
+    "amazonaws.com",
+    "amazonaax.com",
+    "amazonclix.com",
+    "assoc-amazon.com",
+    "ads.google.com",
+    "pagead2.googlesyndication.com",
+    "pagead2.googleadservices.com",
+    "amazon-adsystem.com",
+    "googleadservices.com",
+    "doubleclick.net",
+    "ad.doubleclick.net",
+    "static.doubleclick.net",
+    "m.doubleclick.net",
+    "mediavisor.doubleclick.net",
+    "googleads.g.doubleclick.net",
+    "adclick.g.doubleclick.net",
+    "carbonads.net",
+    "advertising.amazon.com",
+    "advertising.amazon.ca",
+    "google-analytics.com",
+    "doubleclick.com",
+    "doubleclick.de",
+    "partner.googleadservices.com",
+    "googlesyndication.com",
+    "zedo.com",
+    "amazon.ae",
+    "amazon.cn",
+    "advertising.amazon.co.jp",
+    "amazon.co.uk",
+    "advertising.amazon.com.au",
+    "advertising.amazon.com.mx",
+    "advertising.amazon.de",
+    "advertising.amazon.es",
+    "advertising.amazon.fr",
+    "advertising.amazon.in",
+    "advertising.amazon.it",
+    "advertising.amazon.sa",
+    "bingads.microsoft.com",
+    "adcash.com",
+    "taboola.com",
+    "outbrain.com",
+    "smartyads.com",
+    "popads.net",
+    "adpushup.com",
+    "trafficforce.com",
+    "adsterra.com",
+    "creative.ak.fbcdn.net",
+    "adbrite.com",
+    "exponential.com",
+    "quantserve.com",
+    "scorecardresearch.com",
+    "propellerads.com",
+    "admedia.net",
+    "admedia.com",
+    "bidvertiser.com",
+    "undertone.com",
+    "web.adblade.com",
+    "revenuehits.com",
+    "infolinks.com",
+    "vibrantmedia.com",
+    "ads.yahoosmallbusiness.com",
+    "ads.yahoo.com",
+    "hilltopads.net",
+    "clickadu.com",
+    "citysex.com",
+    "ad-maven.com",
+    "propelmedia.com",
+    "enginemediaexchange.com",
+    "advertisers.adversense.com",
+    "a.adtng.com",
+    "ads.facebook.com",
+    "an.facebook.com",
+    "analytics.facebook.com",
+    "pixel.facebook.com",
+    "ads.youtube.com",
+    "youtube.cleverads.vn",
+    "ads-twitter.com",
+    "ads-api.twitter.com",
+    "advertising.twitter.com",
+    "ads.linkedin.com",
+    "analytics.pointdrive.linkedin.com",
+    "ads.reddit.com",
+    "d.reddit.com",
+    "rereddit.com",
+    "events.redditmedia.com",
+    "analytics.tiktok.com",
+    "ads.tiktok.com",
+    "analytics-sg.tiktok.com",
+    "ads-sg.tiktok.com"
+];
+
+// Main Proxy Auto-Configuration Function
 function FindProxyForURL(url, host) {
+    host = host.toLowerCase();
+    url = url.toLowerCase();
+
+    if (debug) {
+        alert("Checking...\nURL: " + url + "\nHost: " + host);
+    }
+
+    // Toggle ad-blocking on/off via special URLs
+    if (host === "antiad.on") {
+        isEnabled = 1;
+        if (debug) alert("Ad-blocking enabled");
+        return blackhole;
+    } else if (host === "antiad.off") {
+        isEnabled = 0;
+        if (debug) alert("Ad-blocking disabled");
+        return blackhole;
+    }
+
+    if (!isEnabled) {
+        return normal;
+    }
+
+    // Local network bypass
+    if (isPlainHostName(host) ||
+        shExpMatch(host, "10.*") ||
+        shExpMatch(host, "172.16.*") ||
+        shExpMatch(host, "192.168.*") ||
+        shExpMatch(host, "127.*") ||
+        dnsDomainIs(host, ".local")) {
+        return normal;
+    }
+
+    // YouTube routing: Send to JS injection proxy FIRST (before ad checks)
     if (shExpMatch(host, "*.youtube.com") ||
         shExpMatch(host, "*.youtu.be") ||
-        shExpMatch(host, "youtube.com") ||
-        shExpMatch(host, "youtu.be")) {
-        return "PROXY 127.0.0.1:$Port";
+        host === "youtube.com" ||
+        host === "youtu.be") {
+        if (debug) alert("YouTube traffic routed to JS injection proxy");
+        return youtubeProxy;
     }
-    return "DIRECT";
+
+    // Whitelist check
+    for (var i = 0; i < whitelist.length; i++) {
+        if (shExpMatch(host, whitelist[i])) {
+            if (debug) alert("Whitelisted: " + host);
+            return normal;
+        }
+    }
+
+    // Ad-blocking logic for all other traffic
+    if (
+        adDomainRegex.test(host) ||
+        adUrlRegex.test(url) ||
+        adSubdomainRegex.test(host) ||
+        adWebBugRegex.test(url) ||
+        blacklist.indexOf(host) !== -1
+    ) {
+        if (debug) alert("Blocked...\nURL: " + url + "\nHost: " + host);
+        return blackhole;
+    }
+
+    if (debug) alert("Not Blocked...\nURL: " + url + "\nHost: " + host);
+    return normal;
+}
+
+if (debug) {
+    alert("Ad-blocking PAC file with YouTube JS injection loaded, isEnabled = " + isEnabled);
 }
 "@
 
@@ -2138,8 +2352,9 @@ public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntP
         } catch {}
 
         $results += "[YouTubeAdBlocker] Proxy server started on port $Port"
-        $results += "[YouTubeAdBlocker] PAC file configured for YouTube-only proxying"
-        $results += "[YouTubeAdBlocker] Ad blocking active for YouTube traffic"
+        $results += "[YouTubeAdBlocker] PAC file configured with comprehensive ad-blocking"
+        $results += "[YouTubeAdBlocker] YouTube traffic: JS injection proxy (port 8080)"
+        $results += "[YouTubeAdBlocker] All other ads: Blackhole proxy (port 3421)"
 
     } catch {
         $results += "[YouTubeAdBlocker] ERROR: $($_.Exception.Message)"
@@ -2147,6 +2362,7 @@ public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntP
 
     return $results
 }
+
 
 # ===================== Main =====================
 
